b717a88885608ebeb386731e227fed03bc8b90ef
rileyeh <rhatchdev@gmail.com>
2021-06-14 20:56:19 -0700

    rollbar

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const path = require('path')
-
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+    accessToken: '92e8deb0bbd94fc99faf5813faa695bb',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  })
 const app = express()
 
 app.use('/styles', express.static(path.join(__dirname, '/public/styles.css')))
@@ -9,8 +14,24 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-app.get('/favicon.ico', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/favicon.ico'))
+let waterObj = {
+    monday: 0, 
+    tuesday: 0,
+    wednesday: 0,
+    thursday: 0,
+    friday: 0,
+    saturday: 0,
+    sunday: 0
+}
+
+app.post('/api/water', (req, res) => {
+    const {selectedDay, waterAmount} = req.body
+    selectedDay = selectedDay.toLowerCase()
+    if (waterObj[selectedDay] === 0) {
+        waterObj[selectedDay] = +waterAmount
+    } else {
+        rollbar.log('no can do')
+    }
 })
 
 const port = process.env.PORT || 5555
