16f2fe82b01d1eb382d2d7947ce37a9a2c6b70c8
rileyeh <rhatchdev@gmail.com>
2021-06-14 20:43:11 -0700

    changed favicon serving

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const path = require('path')
 const app = express()
 
 app.use('/styles', express.static(path.join(__dirname, '/public/styles.css')))
-app.use('/favicon.ico', express.static(path.join(__dirname, '/public/favicon.ico')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/favicon.ico'))
 })
 
 const port = process.env.PORT || 5555
