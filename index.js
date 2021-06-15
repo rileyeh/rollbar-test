83e7a076b97efed3a55aaf4983426bf93f67490d
rileyeh <rhatchdev@gmail.com>
2021-06-14 22:25:40 -0700

    cleaned up

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
 let Rollbar = require('rollbar')
 let rollbar = new Rollbar({
     accessToken: '92e8deb0bbd94fc99faf5813faa695bb',
@@ -9,7 +8,6 @@ let rollbar = new Rollbar({
   })
 const app = express()
 
-app.use(cors())
 app.use(express.json())
 app.use('/styles', express.static(path.join(__dirname, '/public/styles.css')))
 
@@ -22,10 +20,15 @@ let students = []
 app.post('/api/student', (req, res) => {
     let {name} = req.body
     const index = students.findIndex(stu => stu === name)
-    if (index === -1) {
+    name = name.trim()
+    if (index === -1 && name !== '') {
         students.push(name)
-        rollbar.log('student successfully added')
+        rollbar.info('student successfully added', {type: 'manual', author: 'riley'})
         res.status(200).send(students)
+    } else if (name === '') {
+        let errMsg = 'no name given'
+        rollbar.error(errMsg)
+        res.status(400).send(errMsg)
     } else {
         let errMsg = 'student already exists'
         rollbar.error(errMsg)
